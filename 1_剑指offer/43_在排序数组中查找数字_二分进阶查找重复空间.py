a557cd1b13f2caf020be3a42e3b1971e639287af
wang <836691447@qq.com>
2020-05-08 15:18:00 +0800

    二分进阶再看看

---
+++ 1_剑指offer/43_在排序数组中查找数字_二分进阶查找重复空间.py
@@ -0,0 +1,43 @@
+# 利用二分查找特性，寻找左右边界。
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] <= target: i = m + 1
+            else: j = m - 1
+        right = i
+
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = (i + j) // 2
+            if nums[m] < target: i = m + 1
+            else: j = m - 1
+        left = j
+        
+        return right - left - 1
+'''
+统计一个数字在排序数组中出现的次数。
+
+ 
+
+示例 1:
+
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: 2
+示例 2:
+
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: 0
+ 
+
+限制：
+
+0 <= 数组长度 <= 50000
+
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
