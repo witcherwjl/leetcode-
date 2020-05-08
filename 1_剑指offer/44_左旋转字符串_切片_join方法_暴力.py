2da643371389f8e4fcf74d53e6b5244fbfcfbbf3
wang <836691447@qq.com>
2020-05-08 15:50:28 +0800

    方法很多，研究研究

---
+++ 1_剑指offer/44_左旋转字符串_切片_join方法_暴力.py
@@ -0,0 +1,55 @@
+class Solution(object):
+    def reverseLeftWords(self, s, n):
+        """
+        :type s: str
+        :type n: int
+        :rtype: str
+        """
+        if n > len(s) or not s:
+            return ''
+        return ''.join([s[n:],s[:n]])
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        return s[n:] + s[:n]
+
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = []
+        for i in range(n, len(s)):
+            res.append(s[i])
+        for i in range(n):
+            res.append(s[i])
+        return ''.join(res)
+        
+class Solution:
+    def reverseLeftWords(self, s: str, n: int) -> str:
+        res = ""
+        for i in range(n, len(s)):
+            res += s[i]
+        for i in range(n):
+            res += s[i]
+        return res
+'''
+字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+
+ 
+
+示例 1：
+
+输入: s = "abcdefg", k = 2
+输出: "cdefgab"
+示例 2：
+
+输入: s = "lrloseumgh", k = 6
+输出: "umghlrlose"
+ 
+
+限制：
+
+1 <= k < s.length <= 10000
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/zuo-xuan-zhuan-zi-fu-chuan-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
